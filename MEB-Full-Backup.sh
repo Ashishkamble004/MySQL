b5decf83153a59e90680455020492cc3468a3e92
Ashish Kamble <kambleashish004@gmail.com>
2021-08-24 06:49:59 +0530

    Full Backup Script for MySQL DB on EC2, using MEB
    Full Backup Script for MySQL DB on EC2, using MEB(MySQL Enterprise Backup)

---
+++ MEB-Full-Backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIR='/home/mysql/SCRIPTS'
+. $DIR/.DB_PARAM
+
+MYSQL_BASE=/appl/mysql
+export MYSQL_BASE
+
+MYSQL_HOME=/appl/mysql/8.0.22
+
+export MYSQL_HOME
+
+PATH=$PATH:$MYSQL_HOME:$MYSQL_HOME/bin
+export PATH
+
+
+VARDATE=`date +%Y%m%d`
+export VARDATE
+
+mysqlbackup --defaults-file=/appl/mysql/8.0.22/my.cnf -u$pid -p$pidpw --backup_dir=/backup/dbbackup/daily --backup-image=db_full_backup_$VARDATE.mbi --with-timestamp backup-to-image --compress --compress-method=zlib --skip-binlog
+
+find /backup/dbbackup/daily -mtime +15 -type d -exec rm -rf {} \;
